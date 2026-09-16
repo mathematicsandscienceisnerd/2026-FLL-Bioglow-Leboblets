50ea1903d788aeb6390e3a3cd50c590872cf6e82
Samuel Rea <samuel.rea@student.education.wa.edu.au>
2026-09-16 15:25:13 +0800

    Added quotation marks arround the git push -u origin main command, so that it could be seen easily.

--- FLL.py
+++ FLL.py
@@ -1,7 +1,7 @@
 #Run this before running the program.
 #runfll - it is an alias to open the virtual environment
 
-#Once you commit, type git push -u origin main into the terminal to publish.
+#Once you commit, type " git push -u origin main " into the terminal to publish.
 
 from pybricks.hubs import PrimeHub
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
